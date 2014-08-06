5030c015284286feb7d13546a11cd3af68f7a134
Mharcos Nesster <mh4root@gmail.com>
2014-08-06 15:59:02 -0300

    Update decode.asm

--- decode.asm
+++ decode.asm
@@ -1,4 +1,8 @@
 # http://chmodsecurity.com.br/artigo/20/shellcode-polimorficos-chamdos-motor-polymorphic.html
+# nasm -f elf decoder.asm
+# ld -o decoder decoder.o
+# od2sc decoder
+#"\xeb\x10\x5e\x31\xc9\xb1\x00\x80\x6c\x0e\xff\x00\xfe\xc9\x75\xf7\xeb\x05\xe8\xeb\xff\xff\xff" <-----  Nosso Decoder :D 
 BITS 32
 jmp short jmptrick // 
 decoder:
