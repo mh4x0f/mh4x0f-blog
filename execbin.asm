faf7e6b2ed05208621e18a2db565784380735f1e
Mharcos Nesster <mh4root@gmail.com>
2015-01-23 10:54:22 -0300

    Update execbin.asm

--- execbin.asm
+++ execbin.asm
@@ -3,14 +3,14 @@
 xorl% ebx,% ebx
 mul% ebx
 push% edx
-pushl $ 0x3268732f /bin/sh 
+pushl $ 0x3268732f #/bin/sh 
 pushl $ 0x6e69622f 
 mov% esp,% ebx
 push% edx
 push% ebx
 mov% esp,% ecx
 movb $ 0xB,% al # execve () syscall, não retornar a menos que ele falhar, caso em que ele retorna -1
-int $ 0x80 //syscall
-//syscall exit() ExitProcess 
+int $ 0x80 #syscall
+#syscall exit() ExitProcess 
 divl% eax # -1 / -1 = 1 # Aqui a Magica acontece :D 
 int $ 0x80 #
